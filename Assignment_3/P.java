e6102628ed9de6bad92686352179e8505ea787e6
SANJAY4A9 <61999923+SANJAY4A9@users.noreply.github.com>
2020-07-26 20:56:12 +0530

    Create P.java

---
+++ Assignment_3/P.java
@@ -0,0 +1,13 @@
+package packagep1;
+
+public class P 
+{
+	public static void display()
+  {
+		System.out.println("Class P invoked");
+	}
+	public static void main(String a[]) {
+		P.display();
+	}
+	
+}
