8c2a518ade8a76bd0747b8e7f0a925a4572af473
SANJAY4A9 <61999923+SANJAY4A9@users.noreply.github.com>
2020-07-26 21:08:56 +0530

    Create Class_B.java

---
+++ Assignment_3/Class_B.java
@@ -0,0 +1,8 @@
+package packagep2;
+import packagep1.A;
+public class B {
+	public static void main(String a[]) {
+		System.out.println("We are in Package P2 which contain class B ");
+		A.display();
+	}
+}
