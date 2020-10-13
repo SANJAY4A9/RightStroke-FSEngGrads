7f399dea12875e4a09a1a79dd0aae47d7dcac176
SANJAY4A9 <61999923+SANJAY4A9@users.noreply.github.com>
2020-10-13 20:20:34 +0530

    Update app.component.ts

--- Angular_Assignments_2/Forex_calculation_service/app.component.ts
+++ Angular_Assignments_2/Forex_calculation_service/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit} from '@angular/core';
-import {ForexserviceService} from './Forex_calculation_service';
+import {ForexserviceService} from './forexservice.service';
 
 @Component({
   selector: 'app-root',
