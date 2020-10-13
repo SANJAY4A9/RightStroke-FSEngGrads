a2f0c6037d0b562efc29d7b5c9540d33972daaf3
SANJAY4A9 <61999923+SANJAY4A9@users.noreply.github.com>
2020-10-13 20:34:27 +0530

    Create app.module.ts

---
+++ Angular_Assignments_2/Payment_payload_service/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {ForexserviceService} from './forexservice.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
+import { HttpClientModule} from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChildComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [ForexserviceService],
+  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
