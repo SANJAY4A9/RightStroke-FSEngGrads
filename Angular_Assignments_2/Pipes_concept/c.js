42208525c48bb6e90513142aeb85194cef964b39
SANJAY4A9 <61999923+SANJAY4A9@users.noreply.github.com>
2020-10-16 14:18:26 +0530

    Create c.js

---
+++ Angular_Assignments_2/Pipes_concept/c.js
@@ -0,0 +1,7 @@
+function addHyphen (element) 
+{
+  let ele = document.getElementById(element.id);
+  ele = ele.value.split('-').join('');   
+  let finalVal = ele.match(/.{1,4}/g).join('-');
+  document.getElementById(element.id).value = finalVal;
+}
